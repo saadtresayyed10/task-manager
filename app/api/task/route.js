0d2c12f6158765adeb27206ef1c4a89b41ea0811
unknown <saadsyed950@gmail.com>
2024-07-21 09:10:44 +0530

    Implementating REST

--- app/api/task/route.js
+++ app/api/task/route.js
@@ -3,8 +3,14 @@ import Task from "@/model/task";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { title, description } = await request.json();
+  const { title, description, dueDate, priority } = await request.json();
   await connectToDB();
-  await Task.create({ title, description });
+  await Task.create({ title, description, dueDate, priority });
   return NextResponse.json({ message: "Task created" }, { status: 201 });
+}
+
+export async function GET() {
+  await connectToDB();
+  const tasks = await Task.find();
+  return NextResponse.json({ tasks });
 }
